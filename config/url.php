7e1a29ef68860262c1776cf1b09f442cdfd53934
Christian Nelson <cnelson@Christian-Nelsons-MacBook-Pro.local>
2017-12-09 21:25:11 +0200

    url correction

--- config/url.php
+++ config/url.php
@@ -11,6 +11,8 @@ return [
     //"staticSiteUrl" => null,
     //"staticBaseUrl" => null,
     //"scriptName"    => null,
-    "urlType"       => self::URL_CLEAN,
+    //"urlType"       => self::URL_CLEAN,
+    "urlType"         => \Anax\Url\CUrl::URL_CLEAN,
+];
 
 ];
